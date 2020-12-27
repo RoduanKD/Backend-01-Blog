ccaf897e792db1c9038c177be57872ea36c91b70
Roduan Kareem Aldeen <roduan98@gmail.com>
2020-12-27 18:44:56 +0200

    Create PostController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'pages.welcome');
 Route::view('/about', 'pages.about');
 Route::view('/contact', 'pages.contact');
-
+Route::get('/posts', [PostController::class, 'index']);
