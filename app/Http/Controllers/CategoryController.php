6be0aee1eedfca01baaf04a36a05d6560600bc64
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2021-01-28 10:14:30 +0200

    -implement category index method -create the category index view -implement category show method -create the category show view -implement category edit method -create the category edit view -edit the Post update method -implement the category update method -implement the category destroy method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,6 +15,9 @@ class CategoryController extends Controller
     public function index()
     {
         // TODO implement this
+        $categories = Category::paginate(6);
+
+        return view('categories.index', ['categories' => $categories]);
     }
 
     /**
@@ -36,17 +39,17 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name'             => 'required|string|min:2|max:255',
+            'name'             => 'required|string|min:2|max:255|unique:categories',
             'description'      => 'required|string|min:15',
         ]);
 
         $category = Category::create($request->only(['name', 'description']));
-        
+
         if ($category)
             request()->session()->flash('success', 'Category was created successfully.');
         else
             request()->session()->flash('danger', 'Something went wrong.');
-        
+
         return redirect()->route('home');
     }
 
@@ -60,6 +63,8 @@ class CategoryController extends Controller
     {
         // TODO implement this
         // TODO Create the view
+        $category ->firstOrFail();
+        return view('categories.show', ['category' => $category]);
     }
 
     /**
@@ -72,6 +77,9 @@ class CategoryController extends Controller
     {
         // TODO implement this
         // TODO Create the view
+
+
+        return view('categories.edit', ['category' => $category]);
     }
 
     /**
@@ -84,6 +92,19 @@ class CategoryController extends Controller
     public function update(Request $request, Category $category)
     {
         // TODO implement this
+        $request->validate([
+            'name'             => 'required|string|min:2|max:255|unique:categories',
+            'description'      => 'required|string|min:15',
+        ]);
+
+        $category = Category::create($request->only(['name', 'description']));
+
+        if ($category)
+            request()->session()->flash('success', 'Category was updated successfully.');
+        else
+            request()->session()->flash('danger', 'Something went wrong.');
+
+        return redirect()->route('home');
     }
 
     /**
@@ -95,5 +116,10 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         // TODO implement this
+        $category = Category::destroy($category->id);
+
+        request()->session()->flash('danger', 'Category was Deleted successfully.');
+
+        return redirect()->route('categories.index');
     }
 }
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,28 +1,25 @@
 @extends('layouts.app')
 
-@section('title', $post->title)
+@section('name', $category->name)
 
 @section('content')
     <div class="container">
         <div class="columns">
             <div class="column is-6">
-                <img src="{{ $post->featured_image }}" alt="{{ $post->title }}">
-            </div>
-            <div class="column is-6">
-                <h3 class="title is-3">{{ $post->title }}</h3>
+                <h3 class="name is-3">{{ $category->name }}</h3>
                 <div class="content">
-                    {{ $post->content }}
+                    {{ $category->description }}
                 </div>
                 <p class="content">
-                    posted at: {{ $post->created_at }}
+                    Created at: {{ $category->created_at }}
                 </p>
             </div>
         </div>
-        <a href="{{ route('posts.edit', $post->id) }}" class="button is-primary">Edit Post</a>
-        <form action="{{ route('posts.destroy', $post->id) }}" method="post">
+        <a href="{{ route('categories.edit', $category->id) }}" class="button is-primary">Edit Category</a>
+        <form action="{{ route('categories.destroy', $category->id) }}" method="post">
             @csrf
             <input type="hidden" name="_method" value="DELETE">
-            <input class="button is-danger" type="submit" value="Delete Post">
+            <input class="button is-danger" type="submit" value="Delete category">
         </form>
     </div>
 @endsection
