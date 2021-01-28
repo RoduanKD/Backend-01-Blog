f7761487d737da5c1d6ff4b8f9843a672ce47aa5
Ghofran <www.ghofranabdullah818@gmail.com>
2021-01-28 15:10:48 +0200

    changes on category views(edit,index,show) implement the category  methods

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -5,24 +5,17 @@
 @section('content')
 <div class="container">
   <div class="columns is-multiline">
-    @foreach ($posts as $post)
+    @foreach ($categories as $category)
     <div class="column is-4">
-      <a href="{{ route('posts.show', $post->slug) }}">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="{{ $post->featured_image }}" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
+      <a href="{{ route('categories.show', $category->id) }}">
+
               <div class="media-content">
-                <p class="title is-4">{{ $post->title }}</p>
+                <p class="title is-4">{{ $category->name }}</p>
               </div>
-            </div>
+           
         
             <div class="content">
-              {{ $post->content }}
+              {{ $category->description }}
             </div>
           </div>
         </div>
@@ -30,6 +23,6 @@
     </div>
     @endforeach
   </div>
-  {{ $posts->links() }}
+  {{ $category->links() }}
 </div>
 @endsection
