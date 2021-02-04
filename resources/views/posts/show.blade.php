6003ca8e4fb9739341d2de53a677f47f5a29e4fe
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-02-04 17:25:58 +0200

    fix delete button position

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -28,7 +28,7 @@
             <div class="column is-2">
               <a href="{{ route('posts.edit', $post->id) }}" class="button is-primary">Edit Post</a>
             </div>
-            <div class="culomn is-2">
+            <div class="column is-2">
                 <form action="{{ route('posts.destroy', $post->id) }}" method="post">
                     @csrf
                     <input type="hidden" name="_method" value="DELETE">
